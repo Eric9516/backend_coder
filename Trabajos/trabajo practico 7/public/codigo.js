a818450a81a594aa8cd370dffb6bc95c18438701
Eric Cantoni <eric-995-@hotmail.com>
2022-11-24 12:41:22 -0300

    trabajo practico 7

---
+++ Trabajos/trabajo practico 7/public/codigo.js
@@ -0,0 +1,55 @@
+const div = document.getElementById("search");
+const btn_enviar = document.getElementById("submit");
+
+//Obtener producto con ID
+btn_enviar.addEventListener("click", () => {
+	const id = document.getElementById("id").value;
+	fetch(`http://localhost:8080/api/products/${+id}`)
+		.then((res) => res.json())
+		.then((res) => {
+			if (res.product) {
+				return (div.innerHTML = `
+				<p>${res.product.title}</p>
+				<p>${res.product.price}</p>
+				<p>${res.product.id}</p>
+				<img src="${res.product.image}"/>
+				`);
+			} else if (res.error === true) {
+				return (div.innerHTML = `<p>El id ${id} no pertenece a ningun producto</p>`);
+			}
+		});
+});
+
+//________________________________________________________
+
+//Borrar producto con ID
+const btn_delete = document.getElementById("delete");
+
+btn_delete.addEventListener("click", () => {
+	const del = document.getElementById("deleteInput").value;
+	fetch(`http://localhost:8080/api/products/${+del}`, {
+		method: "DELETE",
+	})
+		.then((res) => res.json())
+		.then((res) => console.log(`${res} was deleted`));
+	window.location.reload();
+});
+
+//________________________________________________________
+
+//Modificar archivo agregado
+const btn_modify = document.getElementById("botonModificar");
+btn_modify.addEventListener("click", () => {
+	const idMod = document.getElementById("id_modify").value;
+	const title = document.getElementById("modify_title").value;
+	const price = document.getElementById("modify_price").value;
+	const thumbnail = document.getElementById("modify_thumbnail").value;
+	fetch(`http://localhost:8080/api/products/${+idMod}`, {
+		method: "PUT",
+		body: JSON.stringify({ title: title, price: price, thumbnail: thumbnail }),
+	})
+		.then((res) => res.json())
+		.then((res) => {
+			console.log(res);
+		});
+});
---
+++ Trabajos/trabajo practico 7/server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const { Router } = express;
+const routerProducts = Router();
+const routerCart = Router();
+const app = express();
+const Container = require("./containers/productContainer");
+const container = new Container();
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+	console.log(`Servidor http escuchando en el puerto http://localhost:${PORT}`);
+});
+
+app.set("view engine", "ejs");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api/products", routerProducts);
+app.use("/api/cart", routerCart);
+
+app.get("/", (req, res) => {
+	res.render("pages/inicio");
+});
+
+app.get("/form", (req, res) => {
+	res.render("pages/form");
+});
+
+app.get("/modify", (req, res) => {
+	res.render("pages/modify");
+});
+
+routerProducts.get("/", async (req, res) => {
+	const productList = await container.getAll();
+	res.render("pages/products", { products: productList });
+});
+
+routerProducts.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	const product = await container.getById(id);
+	if (product) {
+		res.send({ success: true, product: product });
+	} else {
+		res.json({ error: true, msj: "id no encontrado" });
+	}
+});
+
+routerProducts.post("/", async (req, res) => {
+	const body = req.body;
+	try {
+		const newProduct = await container.save(body);
+		res.render("pages/form");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+routerProducts.put("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const { title, price, thumbnail } = req.body;
+		await container.updateById(id, title, price, thumbnail);
+		res.json("El producto se actualizo correctamente");
+	} catch (error) {
+		res.json({ error: "Producto no encontrado" });
+	}
+});
+
+routerProducts.delete("/:id", async (req, res) => {
+	const { id } = req.params;
+	await container.deleteById(id);
+	res.json("Producto borrado");
+});
